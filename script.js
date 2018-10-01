663035aebbc08ff717e188a6bf49c15b1847880e
Gabriel Aunan <spidy2603@hotmail.com>
2018-10-01 11:15:22 +0200

    6

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/html'));
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 	res.sendFile(path.join(__dirname, 'script'));
+	res.sendFile(path.join(__dirname, 'html'));
 	
 });
 app.listen(process.env.PORT || 8080);	
