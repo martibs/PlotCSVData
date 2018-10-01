060004331c73361bb4f97f7a09b41a9b33d8ad55
Gabriel Aunan <spidy2603@hotmail.com>
2018-10-01 09:11:32 +0200

    2

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const csvdata = require('csvdata')
+//const csvdata = require('csvdata')
 
 app.use(express.static(__dirname + '/script'));
+app.use(express.static(__dirname + '/html'));
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 	res.sendFile(path.join(__dirname, 'script'));
 	
 });
-app.listen(process.env.PORT || 7000);	
+app.listen(process.env.PORT || 8080);	
